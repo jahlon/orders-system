6975e70403d9c5afd0cd78cc11873930de05ebab
Jesus Hincapie <jahlon@gmail.com>
2023-11-23 13:44:26 -0500

    Add class to represent data repository

---
+++ app/data/repository.py
@@ -0,0 +1,32 @@
+import os
+
+from pymongo import MongoClient
+from dotenv import load_dotenv
+
+load_dotenv()
+
+user = os.getenv('DB_USER')
+password = os.getenv('DB_PASSWORD')
+host = os.getenv('DB_HOST')
+db_name = os.getenv('DB_NAME')
+
+
+class OrdersSystemRepository:
+    def __init__(self):
+        uri = f"mongodb+srv://{user}:{password}@{host}/?retryWrites=true&w=majority"
+        self.__client = MongoClient(uri)
+        self.__db = self.client.get_database(db_name)
+
+    def get_collection(self, collection_name):
+        return self.db[collection_name]
+
+    @property
+    def db(self):
+        return self.db
+
+    @property
+    def client(self):
+        return self.client
+
+    def close(self):
+        self.client.close()
