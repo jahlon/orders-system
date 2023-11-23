fcc8dc5554253f9a875f3f1db2d09e5ac94522f5
Jesus Hincapie <jahlon@gmail.com>
2023-11-23 13:44:31 -0500

    define functions to extract object scheme from database response

---
+++ app/data/schemas.py
@@ -0,0 +1,19 @@
+def product_schema(product) -> dict:
+    return {
+        'id': str(product['_id']),
+        'name': product["name"],
+        'sku': product['sku'],
+        'price': product['price'],
+        'description': product['description']
+    }
+
+
+def order_schema(order) -> dict:
+    return {
+        'id': str(order['_id']),
+        'products': order['products'],
+        'total': order['total'],
+        'status': order['status'],
+        'created_at': order['created_at'],
+        'updated_at': order['updated_at']
+    }
