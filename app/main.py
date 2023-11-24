c31c6b515953e5f0def07c59f10fe4871e6b3df6
jahlon <jahlon@gmail.com>
2023-11-23 21:02:46 -0500

    Add API documentation in main app and include products routes

--- app/main.py
+++ app/main.py
@@ -1,13 +1,59 @@
 from fastapi import FastAPI
 
-app = FastAPI()
+from app.routers import products
+
+description = """
+Orders System API simulates a simple order management system. 
+It allows you to create, update, delete and get orders and products.
+It is intended to be used as a sample project for learning design patterns and principles to
+design and implement maintainable and reusable software.
+
+## Products
+Products are the items that are sold by the system.
+You will be able to:
+- Create a product
+- Update a product
+- Delete a product
+- Get a product by sku
+- Get all products
+
+## Orders
+Orders are the main entity of the system.
+You will be able to:
+- Create an order
+- Get an order by id
+- Get all orders
+"""
 
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
+tags_metadata = [
+    {
+        "name": "products",
+        "description": "Operations related to products",
+    },
+    {
+        "name": "orders",
+        "description": "Operations related to orders",
+    },
+]
+
+app = FastAPI(
+    title="Orders System API",
+    description=description,
+    version="1.0.0",
+    contact={
+        "name": "Jesús Andrés Hincapié",
+        "email": "jahlon@gmail.com"
+    },
+    license_info={
+        "name": "MIT",
+        "url": "https://opensource.org/licenses/MIT"
+    },
+    openapi_tags=tags_metadata,
+    openapi_url="/api/v1/openapi.json",
+    docs_url="/api/v1/docs",
+    redoc_url="/api/v1/redoc",
+)
 
 
-@app.get("/hello/{name}")
-async def say_hello(name: str):
-    return {"message": f"Hello {name}"}
+app.include_router(products.router)
