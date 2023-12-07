d04cbf089c0766ed47ebc69229345fd05aeced48
Jesus Hincapie <jahlon@gmail.com>
2023-12-07 16:18:53 -0500

    Create AWSService class and interfaces

--- app/services/aws_service.py
+++ app/services/aws_service.py
@@ -1,34 +1,45 @@
-import os
+from typing import Annotated, Protocol
 
 import boto3
 from botocore.exceptions import ClientError
-from dotenv import load_dotenv
+from fastapi import Depends
 
+from app.config import Settings, get_settings
 from app.data.errors import CouldNotUploadFileError, CouldNotDeleteFileError
 
-load_dotenv()
 
-aws_bucket = os.getenv('AWS_BUCKET')
-aws_region = os.getenv('AWS_REGION')
+class AWSServiceInterface(Protocol):
+    async def upload_file_to_s3(self, file_name: str, file) -> str:
+        ...
 
-
-async def upload_file_to_s3(file_name: str, file, bucket: str = aws_bucket) -> str:
-    s3_client = boto3.client('s3', aws_access_key_id=os.getenv('AWS_KEY'),
-                             aws_secret_access_key=os.getenv('AWS_SECRET'))
-    try:
-        s3_client.upload_fileobj(file, bucket, f"images/{file_name}")
-        url = f"https://{aws_bucket}.s3.{aws_region}.amazonaws.com/images/{file_name}"
-        return url
-    except ClientError as e:
-        raise CouldNotUploadFileError(e)
+    async def delete_file_from_s3(self, file_name: str):
+        ...
 
 
-async def delete_file_from_s3(file_name: str, bucket: str = aws_bucket):
-    s3_client = boto3.client('s3', aws_access_key_id=os.getenv('AWS_KEY'),
-                             aws_secret_access_key=os.getenv('AWS_SECRET'))
-    try:
-        response = s3_client.delete_object(Bucket=bucket, Key=f"images/{file_name}")
-    except ClientError as e:
-        raise CouldNotDeleteFileError(e)
-    else:
-        return response
+class AWSService:
+
+    def __init__(self, setting: Annotated[Settings, Depends(get_settings)]):
+        self.settings = setting
+
+    async def upload_file_to_s3(self, file_name: str, file) -> str:
+        aws_bucket = self.settings.aws_bucket
+        aws_region = self.settings.aws_region
+        s3_client = boto3.client('s3', aws_access_key_id=self.settings.aws_key,
+                                 aws_secret_access_key=self.settings.aws_secret)
+        try:
+            s3_client.upload_fileobj(file, aws_bucket, f"images/{file_name}")
+            url = f"https://{aws_bucket}.s3.{aws_region}.amazonaws.com/images/{file_name}"
+            return url
+        except ClientError as e:
+            raise CouldNotUploadFileError(e)
+
+    async def delete_file_from_s3(self, file_name: str):
+        aws_bucket = self.settings.aws_bucket
+        s3_client = boto3.client('s3', aws_access_key_id=self.settings.aws_key,
+                                 aws_secret_access_key=self.settings.aws_secret)
+        try:
+            response = s3_client.delete_object(Bucket=aws_bucket, Key=f"images/{file_name}")
+        except ClientError as e:
+            raise CouldNotDeleteFileError(e)
+        else:
+            return response
